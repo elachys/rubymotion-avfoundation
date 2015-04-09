1041e80b80ae372919cf145a44222bb61c1e2268
superplussed <xperts@gmail.com>
2015-04-09 10:28:11 -0400

    displays audio and video inputs

--- app/main_window_controller.rb
+++ app/main_window_controller.rb
@@ -3,7 +3,8 @@ class MainWindowController < NSWindowController
 
   outlet :record_label, NSTextField
   outlet :capture_view, NSView
-  outlet :inputs_box, NSBox
+  outlet :audio_input_box, NSBox
+  outlet :video_input_box, NSBox
 
   MOVIE_FILE_NAME = "test_movie"
 
@@ -11,23 +12,40 @@ class MainWindowController < NSWindowController
     @recording = false
     @movie_recorder = MovieRecorder.new(@capture_view, MOVIE_FILE_NAME)
     @record_label_layout = RecordLabelLayout.new(@record_label, @movie_recorder)
+    add_video_device_input
     add_audio_device_input
   end
 
-  def runAgain(sender)
+  def select_audio sender
+    select(self)
+  end
+
+  def select_video sender
     select(self)
   end
 
   def add_audio_device_input
-    popup = NSPopUpButton.alloc.initWithFrame([[10, 15], [178, 24]])
+    popup = NSPopUpButton.alloc.initWithFrame([[7, 6], [178, 24]])
     AVCaptureDevice.devicesWithMediaType(AVMediaTypeAudio).each do |device|
       popup.addItemWithTitle(device.localizedName)
     end
     # popup.selectItemAtIndex(0)
     popup.autoresizingMask = NSViewMinYMargin
     popup.target = self
-    popup.action = :"runAgain:"
-    @inputs_box.contentView.addSubview(popup)
+    popup.action = :"select_audio :"
+    @audio_input_box.contentView.addSubview(popup)
+  end
+
+  def add_video_device_input
+    popup = NSPopUpButton.alloc.initWithFrame([[7, 6], [178, 24]])
+    AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo).each do |device|
+      popup.addItemWithTitle(device.localizedName)
+    end
+    # popup.selectItemAtIndex(0)
+    popup.autoresizingMask = NSViewMinYMargin
+    popup.target = self
+    popup.action = :"select_video:"
+    @video_input_box.contentView.addSubview(popup)
   end
 
   def keyDown(theEvent)
